83eb093d4a55aa52aa385f9efc21a6a11b345cd3
Caio Sousa <caiovms4@outlook.com>
2022-04-01 20:00:55 -0300

    Atividade

---
+++ src/teste/Atv01.java
@@ -0,0 +1,23 @@
+package teste;
+import java.util.Scanner;
+
+public class Atv01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		float nota1, nota2, media;
+		System.out.println("Insira a primeira nota: ");
+		nota1 = sc.nextFloat();
+
+		System.out.println("Insira a segunda nota: ");
+		nota2 = sc.nextFloat();
+		media = (nota1 + nota2) /2;
+		System.out.println("Sua média é: "+media);
+		if (media <7) {
+			System.out.println("REPROVADO!");
+		}else if (media >=7){
+			System.out.println("APROVADO");
+		}
+	}
+
+}
